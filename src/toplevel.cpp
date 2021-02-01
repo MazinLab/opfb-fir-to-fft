9a2371aa0fed7a412cb6afa090104fead73c536a
jebbailey <baileyji@ucsb.edu>
2021-02-01 12:21:00 -0800

    cleanup and simplify, untested

--- src/toplevel.cpp
+++ src/toplevel.cpp
@@ -5,22 +5,7 @@
 using namespace std;
 #endif
 
-/*
- * 	//N_CHAN_PLANE = 256
-	// cycles 0 2 4 6 ... 510, indexes 0-255
-	if (!cycle[0]) A[cycle/2]=groupin;
-	// cycles 1 3 5 ... 255, indexes 0-127
-	else C[cycle/2]=groupin;
 
-
-	if (cycleout < N_CHAN_PLANE) {
-		// 0 ... 255   256 things
-		groupout=A[cycleout];
-	} else {//if (cycleout < 3*N_CHAN_PLANE/2) {
-		// 256 ... 383    128 things, indexes 0-127
-		groupout=C[cycleout-N_CHAN_PLANE+128*(cycleout < 3*N_CHAN_PLANE/2)];
-	}
- */
 void fir_to_fft(pfbaxisword_t &input, pfbaxisword_t &output) {
 //This takes a single PFB lane stream, consisting of 2 sets (one is delayed) of 256 TDM channels,
 // and reorders them, correctly applying the required circular shift.
@@ -34,28 +19,21 @@ void fir_to_fft(pfbaxisword_t &input, pfbaxisword_t &output) {
 #pragma HLS INTERFACE axis register port=output
 #pragma HLS INTERFACE ap_ctrl_none port=return
 
-	static bool primed, bwrite;
-	static ap_uint<9> cycle, cycleout;
+	static bool bwrite;//,primed;
+	static ap_uint<9> cycle;//, cycleout;
 	static iqword_t buffer[2][512];
 #pragma HLS ARRAY_PARTITION variable=buffer complete dim=1
 
-	iqword_t groupin=0, groupout=0;
-
-	groupin=input.data;
 
 	//N_CHAN_PLANE = 256
 	// cycles 0 2 4 6 ... 510, indexes 0-255
 
-	bool firsthalf=(cycle < N_CHAN_PLANE);
+	bool firsthalf=cycle < N_CHAN_PLANE;
 	int ndx=cycle/2+cycle[0]*(256+128*firsthalf-128*!firsthalf);
-	buffer[bwrite][ndx]=groupin;
-	groupout=buffer[!bwrite][cycleout];
-
-	output.data=groupout;
+	buffer[bwrite][ndx]=input.data;
+	output.data=buffer[!bwrite][cycle];
 	output.last=cycle==255 || cycle==511;
 
 	if (cycle==511) bwrite=!bwrite;
-	if (primed) cycleout++;
-	primed|=cycle==511;
 	cycle++;
 }
